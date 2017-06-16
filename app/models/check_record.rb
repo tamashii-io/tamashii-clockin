064d621cac268f978e93227346b6a3d8d5ff2ccc
shes50103 <shes50103@gmail.com>
2017-06-16 16:41:35 +0800

    active scope avoid double checkin

--- app/models/check_record.rb
+++ app/models/check_record.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class CheckRecord < ApplicationRecord
+  MAX_CHECKIN_TIME = 5.minutes
+
   belongs_to :user
+
+  scope :active, -> { where(updated_at: MAX_CHECKIN_TIME.ago..Float::INFINITY) }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :check_records
+
+  def checkin
+    if check_records.active.empty?
+      self.check_records.create!
+      puts "Here!!!"
+    else
+      puts "Wait!!!"
+    end
+  end
+
+  def latest_record
+    check_records.active
+  end
 end
