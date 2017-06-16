173c65bf081acf97ff4fe37bbf232e06abf60f6d
Chelong <chelong821204@gmail.com>
2017-06-16 12:53:01 +0800

    set index root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   devise_for :users
+
+  root 'home#index'
   mount Tamashii::Manager.server => '/tamashii' unless Rails.env.test?
 end
