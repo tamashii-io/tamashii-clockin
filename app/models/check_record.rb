32b7d697ec01ae9db42b199d92be692dc74bd7d2
shes50103 <shes50103@gmail.com>
2017-06-20 16:42:07 +0800

    change type to behavior

--- app/models/check_record.rb
+++ app/models/check_record.rb
@@ -6,8 +6,6 @@ class CheckRecord < ApplicationRecord
   belongs_to :user
   before_save :assign
 
-  self.inheritance_column = :_type_disabled
-
   scope :active, -> { where(updated_at: MAX_CHECKIN_TIME.ago..Float::INFINITY) }
   scope :today, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
 
