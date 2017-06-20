96b90f05eeaed5f54f6b2b46b0e3604dd7161690
shes50103 <shes50103@gmail.com>
2017-06-20 10:20:24 +0800

    rspec step1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:check_records) }
+
+  subject { create(:user) }
+
+  describe '#checkin' do
+    it 'no check_record' do
+      expect { subject.checkin }.to change { CheckRecord.count }.by(1)
+    end
+
+    it 'has check_record exclude -INFINITY..specific.ago' do
+      subject.checkin
+      expect { subject.checkin }.to change { CheckRecord.count }.by(0)
+    end
+  end
 end
