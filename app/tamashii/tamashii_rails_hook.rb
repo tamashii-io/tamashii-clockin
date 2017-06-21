09b90607e7a8142bf06da26c77fb5ae80051d7c1
Chelong <chelong821204@gmail.com>
2017-06-21 16:36:15 +0800

    tamashii connect

--- app/tamashii/tamashii_rails_hook.rb
+++ app/tamashii/tamashii_rails_hook.rb
@@ -9,6 +9,7 @@ class TamashiiRailsHook < Tamashii::Hook
   def initialize(*args)
     super
     @client = @env[:client]
+    puts "fkofjo"
   end
 
   def call(packet)
@@ -24,7 +25,9 @@ class TamashiiRailsHook < Tamashii::Hook
     type, data = case packet.type
                  when Tamashii::Type::RFID_NUMBER
                    # TODO: Crad serial will raded from here
-                   [nil, nil]
+                   json = JSON.parse(packet.body)
+                   packet_id, card_id = [json['id'], json['ev_body']]
+                   [Tamashii::Type::RFID_DATA, {id: packet_id}.to_json]
                  end
     response type, data unless type.nil? || data.nil?
   end
@@ -38,5 +41,17 @@ class TamashiiRailsHook < Tamashii::Hook
 
   def interested?(packet)
     INTERESTED_TYPES.include?(packet.type)
+  end
+
+  def pack(body)
+    {
+      id: packet_id,
+      ev_body: body.to_json
+    }.json
+  end
+
+  def unpack
+    json = JSON.parse(@packet.body)
+    [json['id'], json['ev_body']]
   end
 end
