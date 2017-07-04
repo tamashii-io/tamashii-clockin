717ecde5b64ceaa3d1b1ee3965a43da6e80ab4a2
Chelong <chelong821204@gmail.com>
2017-07-04 15:14:53 +0800

    remove paranoia

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 
   validates :card_serial, uniqueness: { allow_blank: true }
 
