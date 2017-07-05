eb32194278ebe5a4e5f9e8dca2a10bd14d8aed64
Chelong <chelong821204@gmail.com>
2017-07-05 14:31:29 +0800

    fix breadcrumb and describe field

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+crumb :root do
+  link 'Home', root_path
+end
+
+crumb :check_records do
+  link 'Check Records', check_records_path
+end
+
+crumb :users do
+  link 'Users', users_admin_index_path
+end
