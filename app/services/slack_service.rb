da1e5ae5425dc5dc29fbc39d7c9ba71511164986
Chelong <chelong821204@gmail.com>
2017-07-06 17:55:33 +0800

    add slack task

---
+++ app/services/slack_service.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+class SlackService
+  ENDPOINT = 'https://hooks.slack.com/services'
+
+  def initialize(token)
+    @token = token
+  end
+
+  # TODO: Refactor below method
+  # rubocop:disable Metrics/MethodLength
+  def notify(subject, content, status)
+    post(
+      messages_uri(),
+      "text": subject,
+      "attachments": [
+        {
+          "title": status,
+          "fields": [
+            {
+              "title": "已打卡人數",
+              "value": "1",
+              "short": true
+            },
+            {
+              "title": "未打卡人數",
+              "value": "3",
+              "short": true
+            }
+          ],
+          "author_name": content,
+          "author_icon": "http://a.slack-edge.com/7f18/img/api/homepage_custom_integrations-2x.png"
+        }
+      ]
+    )
+  end
+
+  protected
+
+  def messages_uri()
+    URI(ENDPOINT + '/' + @token)
+  end
+
+  def post(uri, data)
+    req = Net::HTTP::Post.new(uri)
+    req.content_type = 'application/json'
+    req.body = data.to_json
+    http(uri) { |http| http.request(req) }
+  end
+
+  def http(uri, &block)
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, &block)
+  end
+end
