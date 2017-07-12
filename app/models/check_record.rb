ff4f8a138d7e93d5ba796fe8b9c0054a1096048e
Chelong <chelong821204@gmail.com>
2017-07-12 12:22:19 +0800

    modify record num per page

--- app/models/check_record.rb
+++ app/models/check_record.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CheckRecord < ApplicationRecord
-  paginates_per 5
+  paginates_per 25
   # TODO: modify for test
   MAX_CHECKIN_TIME = 5.seconds
   default_scope { order(created_at: :desc) }
