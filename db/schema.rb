33f04d906b5a09ceaad26875493ec734f0699c37
Chelong <chelong821204@gmail.com>
2017-11-18 23:36:27 +0800

    add provider and uid column in User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712044138) do
+ActiveRecord::Schema.define(version: 20171118153338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20170712044138) do
     t.boolean "admin", default: false
     t.boolean "deleted", default: false
     t.integer "job_type", default: 0
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
