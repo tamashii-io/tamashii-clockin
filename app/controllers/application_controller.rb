8a130e383968a2d2aa4b78d039954cf6030f6608
Chelong <chelong821204@gmail.com>
2017-11-22 17:27:25 +0800

    add condition in authorize user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
