ee615d137185ac570ade092ac526a610a869f5fa
Chelong <chelong821204@gmail.com>
2017-11-22 17:25:09 +0800

    add omniauth callback

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < ApplicationController
+  def gitlab
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      flash[:unauthenticated] = true
+      flash[:alert] = I18n.t("devise.registrations.already_signup")
+      redirect_to root_path
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
