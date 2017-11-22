af63dedb16daa4a3c2a9d89db178269df5ae388c
Chelong <chelong821204@gmail.com>
2017-11-22 18:26:47 +0800

    fix controller authorize problem

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,9 @@
-class Users::OmniauthCallbacksController < ApplicationController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def gitlab
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      byebug
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
