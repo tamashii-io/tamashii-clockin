c8c98bdcab386b7e9b855e0e464e25efe7f0dec0
jimmy2822 <jimmy2822@gmail.com>
2019-03-25 15:48:27 +0800

    add Model Membership, add job_types student

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712044138) do
+ActiveRecord::Schema.define(version: 20190325073424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170712044138) do
     t.datetime "updated_at", null: false
     t.integer "type", default: 0
     t.integer "registrar_id"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.string "title"
+    t.date "join_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
